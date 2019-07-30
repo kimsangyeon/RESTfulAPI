d683b289e87af27f082eada803399dfc6e9237de
Kim Sangyeon <dho113@naver.com>
2019-07-30 23:14:30 +0900

    -/ init server
    - set ejs template engine
    - render index.html

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 const port = '8080';
 
+// ejs Embedded JavaScript Template
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.render('index.html');
 });
 
 app.listen(port, (err) => {
