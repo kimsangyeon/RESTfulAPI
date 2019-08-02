0d9cfab260a4e8264f8e1a070fc4c2c7d18749ed
Kim Sangyeon <dho113@naver.com>
2019-08-02 22:57:04 +0900

    -/ init server
    - xhr get 작업 롤백

--- app.js
+++ app.js
@@ -12,25 +12,6 @@ app.get('/', (req, res) => {
     res.render('index.html');
 });
 
-app.get('/get/cats', (req, res) => {
-    const cats = {
-        sanji: {
-            name: 'sanji',
-            age: 1
-        }
-    };
-    console.log("params: ", req.params);
-    console.log("query: ", req.query);
-    console.log("body: ", req.body);
-    console.log("headers: ", req.headers);
-
-    if (req.query.name) {
-        res.json(cats[req.query.name]);
-    } else {
-        res.end('cats!');
-    }
-});
-
 app.listen(port, (err) => {
     console.log('connect : ', port);
 
